16c3c82cfc4d17e5c98642abb717a04e4c9f59c4
PeaceCreation <peacecreation2022@gmail.com>
2024-10-27 15:38:21 -0700

    files uploaded now

--- webpack.config.js
+++ webpack.config.js
@@ -2,113 +2,113 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-
-const { option } = require('commander');
-
 module.exports = {
-  // step one
+  // Entry point
   entry: {
     app: './src/index.js'
   },
-  // step 2
+  
+  // Output configuration
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: 'js/main.js',
   },
-  // step four
-    module: {
-      rules: [
-        // load images
-        {
-          test: /\.(png|svg|jpg|jpeg|ttf)$/i,
-          type: 'asset/resource',
-          generator:{
-            filename: "./images/[name][ext]"
-          }
-        },
-        // load Fonts 
-        {
-          test: /\.(svg|eot|woff2|jpeg|gif)$/i,
-          type: 'asset/resource',
-          generator:{
-            filename: "./fonts/[name][ext]"
-          }
-        },
-        {
-          test: /\.html$/i,
-          loader: "html-loader",
-          
-        },
-        {
-          test: /\.css$/i,
-          use: [
-           {
-            loader: MiniCssExtractPlugin.loader,
-              options:{
-                esModule: false
-              }
-           },
-             "css-loader"],
-        },
-        // Sass Loader 
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            // Creates `style` nodes from JS strings
-            "style-loader",
-            // Translates CSS into CommonJS
-            "css-loader",
-            // Compiles Sass to CSS
-            "sass-loader",
-          ],
-        },
-   
-
-      ],
-    },
-    // Server webpack 
-    devServer: {
-      static: {
-        directory: path.join(__dirname, 'dist'),
-      },
-      hot: false,
-      compress: true,
-      port: 9000,
-      devMiddleware: {
-        writeToDisk: true,
-      }
-    },
   
-  // step three
+  // Module rules
+  module: {
+    rules: [
+      // Rule to load images (including .jfif)
+      {
+        test: /\.(png|svg|jpg|jpeg|jfif|webp|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]', // Output folder for images
+        },
+      },
+      
+      // Rule to load fonts
+      {
+        test: /\.(svg|eot|woff2|ttf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]', // Output folder for fonts
+        },
+      },
+
+      // Rule to load HTML files
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+
+      // Rule to load CSS files
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { esModule: false }, // Extracts CSS into separate files
+          },
+          'css-loader', // Translates CSS into CommonJS
+        ],
+      },
+
+      // Rule to load and compile Sass/SCSS files
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader, // Extracts CSS into separate files
+          'css-loader',  // Translates CSS into CommonJS
+          'sass-loader', // Compiles Sass to CSS
+        ],
+      },
+    ],
+  },
+
+  // Webpack DevServer configuration
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    hot: false,
+    compress: true,
+    port: 9000,
+    devMiddleware: {
+      writeToDisk: true, // Write files to disk
+    },
+  },
+
+  // Plugins
   plugins: [
+    // Multiple HTML files to generate
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: './src/index.html'
+      template: './src/index.html',
     }),
     new HtmlWebpackPlugin({
       filename: 'egypt.html',
-      template: './src/egypt.html'
+      template: './src/egypt.html',
     }),
     new HtmlWebpackPlugin({
       filename: 'turkey.html',
-      template: './src/turkey.html'
+      template: './src/turkey.html',
     }),
     new HtmlWebpackPlugin({
       filename: 'contact.html',
-      template: './src/contact.html'
+      template: './src/contact.html',
     }),
     new HtmlWebpackPlugin({
       filename: 'signin.html',
-      template: './src/signin.html'
+      template: './src/signin.html',
     }),
     new HtmlWebpackPlugin({
       filename: 'tours.html',
-      template: './src/tours.html'
+      template: './src/tours.html',
     }),
+
+    // CSS extraction into a separate file
     new MiniCssExtractPlugin({
       filename: 'css/main.css',
     }),
-  
   ],
-
 };
